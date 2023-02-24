c141015c36d5142658dde92bee01ce5e383cb416
Alex Smirnov <alexsmirnov601@gmail.com>
2023-02-24 17:26:38 +0300

    added loader

--- src/components/Stocks.vue
+++ src/components/Stocks.vue
@@ -8,6 +8,7 @@ export default {
       stocks: [],
       errors: [],
       selected: '',
+      isLoading: true,
     }
   },
   created() {
@@ -19,38 +20,43 @@ export default {
         this.stocks = res.data
       })
       .catch((e) => this.errors.push(e))
+      .finally(() => {
+        this.isLoading = false
+      })
   },
 }
 </script>
-
 <template>
-  <div class="stock">
-    <div class="stock-item" v-for="stock in stocks" :key="stock.stock">
-      <div class="stock-item__info">
-        <div class="stock-item__cover">
-          <img :src="stock.image" :alt="stock.companyName" />
+  <div class="loader" v-if="isLoading">Loading...</div>
+  <div v-else>
+    <div class="stock">
+      <div class="stock-item" v-for="stock in stocks" :key="stock.stock">
+        <div class="stock-item__info">
+          <div class="stock-item__cover">
+            <img :src="stock.image" :alt="stock.companyName" />
+          </div>
+          <h3 class="stock-item__title">
+            {{ stock.companyName }}
+            <span>{{ stock.symbol }}</span>
+          </h3>
         </div>
-        <h3 class="stock-item__title">
-          {{ stock.companyName }}
-          <span>{{ stock.symbol }}</span>
-        </h3>
+        <span class="stock-item__price">{{ stock.price }} $</span>
       </div>
-      <span class="stock-item__price">{{ stock.price }} $</span>
     </div>
-  </div>
-  <h3>Get info about company</h3>
-  <select v-model="selected">
-    <option disabled value="">Select Company</option>
-    <option
-      v-for="stock in stocks"
-      :key="stock.stock"
-      :value="stock.description"
-    >
-      {{ stock.companyName }}
-    </option>
-  </select>
-  <div class="info">
-    {{ selected }}
+    <h3>Get info about company</h3>
+    <select v-model="selected">
+      <option disabled value="">Select Company</option>
+      <option
+        v-for="stock in stocks"
+        :key="stock.stock"
+        :value="stock.description"
+      >
+        {{ stock.companyName }}
+      </option>
+    </select>
+    <div class="info">
+      {{ selected }}
+    </div>
   </div>
 </template>
 
